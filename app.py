b1ac88501fafec950083650ef03558fa5f5b4380
Jaswanth Babu Reddi <jr8281@srmist.edu.in>
2026-03-24 23:49:56 +0530

    developers added

--- app.py
+++ app.py
@@ -116,9 +116,9 @@ with st.sidebar:
 
 # ─── Main UI ──────────────────────────────────────────────────────────────────
 st.markdown('<p class="main-title"> Disease Prediction System</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub-title">Developed by</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub-title">Jaswanth Babu Reddi</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub-title">Tanmay Guruvugari</p>', unsafe_allow_html=True)
+st.markdown('<p class="sub-title">Developed by\n Jaswanth Babu Reddi\n Tanmay Guruvugari</p>', unsafe_allow_html=True)
+
+
 
 st.markdown("---")
 
